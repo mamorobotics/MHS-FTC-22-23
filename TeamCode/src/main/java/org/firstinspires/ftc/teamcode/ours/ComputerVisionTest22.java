73829894ba6edc43ef207ba2b15d2d5306a3f32e
maxjlaz1 <maxjlaz@icloud.com>
2022-11-10 23:26:47 -0500

    Update ComputerVisionTest22.java
    fixed some bugs and formatting, also added potential fix to view output of camera on driver station

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/ComputerVisionTest22.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/ComputerVisionTest22.java
@@ -6,7 +6,7 @@
  import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
  import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
- import org.firstinspires.ftc.teamcode.ours.vision.CupDetector;
+ import org.firstinspires.ftc.teamcode.ours.Vision.CupDetectorPowerPlay;
  import org.openftc.easyopencv.OpenCvCamera;
  import org.openftc.easyopencv.OpenCvCameraFactory;
  import org.openftc.easyopencv.OpenCvCameraRotation;
@@ -14,7 +14,7 @@
  @Autonomous(name = "visionTest22")
  public class ComputerVisionTest22 extends LinearOpMode {
      OpenCvCamera webcam; // webcam object
-     CupDetector detector = new CupDetector(telemetry);
+     CupDetectorPowerPlay detector = new CupDetectorPowerPlay(telemetry);
 
      @SuppressLint("DefaultLocale")
      @Override
@@ -35,21 +35,23 @@
 
          });
 
-    while(!opModeIsActive() && !isStopRequested()){
-             int cupSide = detector.getCupPosition(); // gets the pos of the duck
-             String cupColor;
-             if(cupSide == 0){cupColor = "Yellow";}
-             if(cupSide == 1){cupColor = "Blue";}
-             if(cupSide == 2){cupColor = "Green";}
-             else{cupColor = "No Color";}
-             telemetry.addData("Cup Color: ", cupColor);
-             //adds a bunch of data to benchmark pipeline and controller hub
-             telemetry.update();
-             sleep(100);        }
-         waitForStart();
-         int snapshot = detector.getCupPosition();
-         while (opModeIsActive()){
-             telemetry.addData("snapshot analysis", snapshot);
-         }
+        while(!opModeIsActive() && !isStopRequested()){
+            webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT); // This might go here to make DS viewing work
+            int cupSide = detector.getCupPosition(); // gets the pos of the duck
+            String cupColor;
+            if(cupSide == 0){cupColor = "Yellow";}
+            if(cupSide == 1){cupColor = "Blue";}
+            if(cupSide == 2){cupColor = "Green";}
+            else{cupColor = "No Color";}
+            telemetry.addData("Cup Color: ", cupColor);
+            //adds a bunch of data to benchmark pipeline and controller hub
+            telemetry.update();
+            sleep(100);
+        }
+        waitForStart();
+        int snapshot = detector.getCupPosition();
+        while (opModeIsActive()){
+            telemetry.addData("snapshot analysis", snapshot);
+        }
      }
  }
