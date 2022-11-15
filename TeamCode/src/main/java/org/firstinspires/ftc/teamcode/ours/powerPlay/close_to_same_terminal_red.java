a2799830828697307cea649ea17cb239b018ffb6
maxjlaz1 <maxjlaz@icloud.com>
2022-11-15 18:48:04 -0500

    Incorporated more CV into Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/powerPlay/close_to_same_terminal_red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/powerPlay/close_to_same_terminal_red.java
@@ -4,16 +4,40 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.ours.Vision.ConeDetectorPowerPlay;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
-//Autonomous(name = "Close_To")
+//Autonomous(name = "Close_To_Same_Red")
 public class close_to_same_terminal_red extends LinearOpMode {
     Pose2d startPos = new Pose2d(-36, -62, Math.toRadians(270));
     int cupSide = 1;
+    OpenCvCamera webcam;
+    ConeDetectorPowerPlay detector = new ConeDetectorPowerPlay(telemetry);
 
     @Override
     public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        webcam.setPipeline(detector);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+                telemetry.addData("failed to open camera setting position to ", detector);
+            }
+
+        });
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         drive.setPoseEstimate(startPos);
 
@@ -49,7 +73,26 @@ public class close_to_same_terminal_red extends LinearOpMode {
                 .lineToLinearHeading(new Pose2d(-12, -62, Math.toRadians(270)))
                 .build();
 
+        while (!opModeIsActive() && !isStopRequested()) {
+            //bucketServo.setPosition(upright);
+            cupSide = detector.getCopColor();
+            String cupColor = "**Add Colors**";
+            if(cupSide == 0) {
+                cupColor = "0";
+            }
+            if(cupSide == 1) {
+                cupColor = "1";
+            }
+            if(cupSide == 2) {
+                cupColor = "2";
+            }
+            telemetry.addData("Cup Color", cupColor);
+            telemetry.update();
+            sleep(100);
+        }
+
         waitForStart();
+
         drive.followTrajectorySequence(baseSeq);
         if(cupSide == 1) {
             drive.followTrajectorySequence(seq1);
