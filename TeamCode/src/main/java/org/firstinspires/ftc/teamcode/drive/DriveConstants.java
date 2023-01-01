4608ceeab957a1cfd16b58df4c324c5af86279bd
Mustafa Khan <diysplat@gmail.com>
2023-01-01 17:00:30 -0500

    various constraint changes and testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -34,8 +34,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(14, 0, 11,
-            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(16, 0, 0.9, 12.755062915580503);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -46,8 +45,8 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.8898; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 13.50; // in
+    public static double GEAR_RATIO = 1.0200972139; // output (wheel) speed / input (motor) speed
+    public static double TRACK_WIDTH = 13.05; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
